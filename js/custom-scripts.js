c717c87c8866b448145ac6a8800352b0e301cde8
ElanaBolgova <elena.bolgova.90@gmail.com>
2017-12-29 03:50:44 +0200

    home work

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,14 +1,38 @@
 $(window).ready(function() {
 
     setTimeout(function() {
-        $('.wrapper').animate({opacity: 1}, 300);
+        $( '.wrapper' ).animate({ opacity: 1 }, 300 );
     }, 300);
 
-    $('.slider').slick({
+    $( '.slider' ).slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: true,
         infinite: false
+    });
+
+});
+
+$( '.js-dropdown' ).on( 'click', function() {
+
+    var status = 0; //default display none
+
+    if ( status == 0) {
+        $( '.wrapper-dropdown ul' ).css({ 'opacity': '1' });
+        status = 1;
+    } else {
+        $( '.wrapper-dropdown ul' ).css({ 'opacity': '0' });
+        status = 0;
+    }
+
+    event.stopPropagation();
+
+    $( 'body' ).on( 'click', function() {
+        $( '.wrapper-dropdown ul' ).css({ 'opacity': '0' });
+    });
+
+    $( 'body' ).on( 'click', 'a', function () {
+        window.location.href = $( this ).attr( 'href' );
     });
 
 });
